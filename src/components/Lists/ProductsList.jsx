aad9f4f97f2310eee4b7360f6384cfd45264223a
BoJlkogaB <bojlkogab.03@gmail.com>
2022-11-03 16:06:35 +0300

    .

--- src/components/Lists/ProductsList.jsx
+++ src/components/Lists/ProductsList.jsx
@@ -1,24 +1,23 @@
 import React from 'react'
 import './ProductsList.css'
-import Grid2 from '@mui/material/Unstable_Grid2'
-import { Card } from '@mui/material'
+import { Card, Grid } from '@mui/material'
 
 const ProductsList = () => {
   return (
-    <Grid2 container spacing={2}>
-      <Grid2 xs={12}>
+    <Grid container spacing={2}>
+      <Grid xs={12}>
         <Card>xs=8</Card>
-      </Grid2>
-      <Grid2 xs={12}>
+      </Grid>
+      <Grid xs={12}>
         <Card>xs=4</Card>
-      </Grid2>
-      <Grid2 xs={12}>
+      </Grid>
+      <Grid xs={12}>
         <Card>xs=4</Card>
-      </Grid2>
-      <Grid2 xs={12}>
+      </Grid>
+      <Grid xs={12}>
         <Card>xs=4</Card>
-      </Grid2>
-    </Grid2>
+      </Grid>
+    </Grid>
   )
 }
 
