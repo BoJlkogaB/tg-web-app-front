c69da6026d94c37e6d676e30a829b4dddad8ca08
BoJlkogaB <bojlkogab.03@gmail.com>
2022-11-04 09:09:50 +0300

    .

--- src/components/Cards/StandardCard.jsx
+++ src/components/Cards/StandardCard.jsx
@@ -7,27 +7,51 @@ import {
   Grid,
   Typography,
 } from '@mui/material'
+import { useTelegram } from '../../hooks/useTelegram'
 
-const StandardCard = ({name, price, description}) => {
+const StandardCard = ({ name, price, description }) => {
+  const { user } = useTelegram()
+
+  const onSendData = () => {
+    const data = {
+      order: name,
+      userName: user.username,
+      userId: user.id,
+    }
+
+    fetch('http://5.188.139.166:8000/order', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    })
+  }
+
   return (
-      <Grid xs={12}>
-        <Card sx={{ width: 325, margin: 'auto', marginTop: 2, border: '1px solid black' }}>
-          <CardContent>
-            <Typography variant="h6" component="div">
-              {name}
-            </Typography>
-            <Typography sx={{ mb: 1.5 }} color="text.secondary">
-              от {price}₽
-            </Typography>
-            <Typography variant="body2">
-              {description}
-            </Typography>
-          </CardContent>
-          <CardActions>
-            <Button size="small" sx={{ marginLeft: 'auto' }}>Заказать</Button>
-          </CardActions>
-        </Card>
-      </Grid>
+    <Grid xs={12}>
+      <Card sx={{
+        width: 325,
+        margin: 'auto',
+        marginTop: 2,
+        border: '1px solid black',
+      }}>
+        <CardContent>
+          <Typography variant="h6" component="div">
+            {name}
+          </Typography>
+          <Typography sx={{ mb: 1.5 }} color="text.secondary">
+            от {price}₽
+          </Typography>
+          <Typography variant="body2">
+            {description}
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button onClick={onSendData} size="small" sx={{ marginLeft: 'auto' }}>Заказать</Button>
+        </CardActions>
+      </Card>
+    </Grid>
   )
 }
 
